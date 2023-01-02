30eb9d5327edf96a67e4e02aabfe4a3c5cfd812c
Denise De Jesus <denisedeedejesus@pm.me>
2023-01-02 12:26:29 +0800

    added Users, Sessions, Movies, Cinemas

---
+++ app/views/cinemas/_cinema.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! cinema, :id, :created_at, :updated_at
+json.url cinema_url(cinema, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+  get 'sessions/about'
+  resources :cinemas
+  resources :movies
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
