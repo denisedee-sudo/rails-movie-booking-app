c286feaddf8ba9c4f3f79cbb8d27967538bfea55
Denise De Jesus <denisedeedejesus@pm.me>
2023-01-06 13:22:41 +0800

    final countdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 User.create([{ admin: true, first_name: "Miguel", last_name: "Ignazio", email: "mx@email.com", phone: "09760507982", password: "bab"}, { admin: false, first_name: "Jon", last_name: "Doe", email: "jon@email.com", phone: "09275231825", password: "doe"} ])
 Movie.create([{title: "Prestige", year: 2012},{title: "Avengers: Endgame", year: 2019}])
 Cinema.create([{seats: 10 },{seats: 10 }])
-Showing.create([{cinema_id: 1, movie_id: 2, show_time: 2023-01-28 10:30:00},{cinema_id: 2, movie_id: 1, show_time: 2023-01-29 15:30:00}])
-Booking.create([{user_id: 1,showing_id: 1,cinema_id: 1}, {user_id: 2,showing_id: 2,cinema_id: 2}])
+# Showing.create([{cinema_id: 1, movie_id: 2, show_time: 2023-01-28 10:30:00},{cinema_id: 2, movie_id: 1, show_time: 2023-01-29 15:30:00}])
+# Booking.create([{user_id: 1,showing_id: 1,cinema_id: 1}, {user_id: 2,showing_id: 2,cinema_id: 2}])
